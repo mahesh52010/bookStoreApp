eeaeb9b2d24b47ffa11b7de39be82b579f193e2f
mahesh52010 <132468664+mahesh52010@users.noreply.github.com>
2024-11-10 20:55:13 +0530

    Update Signup.jsx

--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -21,8 +21,8 @@ function Signup() {
       password: data.password,
     };
     await axios
-      .post("http://localhost:4001/user/signup", userInfo)
-      .then((res) => {
+    const res = await axios.post("https://bookstoreapp-t2hu.onrender.com/user/signup", userInfo);
+   .then((res) => {
         console.log(res.data);
         if (res.data) {
           toast.success("Signup Successfully");
