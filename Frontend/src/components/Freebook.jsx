80822cb42c182c6f300c9ad54db66ccedf0f2ee3
mahesh52010 <132468664+mahesh52010@users.noreply.github.com>
2024-11-12 22:12:53 +0530

    Update Freebook.jsx

--- Frontend/src/components/Freebook.jsx
+++ Frontend/src/components/Freebook.jsx
@@ -12,7 +12,7 @@ function Freebook() {
   useEffect(() => {
     const getBook = async () => {
       try {
-        const res = await axios.get("https://bookstoreapp-t2hu.onrender.com/book");
+        const res = await axios.get("https://bookstoreapp-b38q.onrender.com/book");
 
         const data = res.data.filter((data) => data.category === "Free");
         console.log(data);
